c531653af7899398abe4900bd0d5444ea35bcd44
Abhishek <kumarabhishek32202@gmail.com>
2025-11-03 23:56:33 +0530

    update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,23 @@ import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
 import Profile from "./Pages/Profile";
 import NotFound from "./Pages/NotFound";
+import PrivateRoute from "./components/PrivateRoute";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
 
           <Route path="*" element={<NotFound />} />
         </Routes>
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { signInSuccess } from "../features/user/userSlice";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function OAuth() {
   const dispatch = useDispatch();
@@ -12,35 +11,34 @@ export default function OAuth() {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
-      const response = await signInWithPopup(auth, provider);
-      console.log("got the resp from google", response);
-      const res = await fetch(
-        "https://plotpe-mern-project.onrender.com/api/auth/google",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            name: response.user.displayName,
-            email: response.user.email,
-            photo: response.user.photoURL,
-          }),
-          credentials: "include",
-        }
-      );
+
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
       const data = await res.json();
       dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      console.log("Error in server communication", error);
+      console.log("could not sign in with google", error);
     }
   };
-
   return (
     <button
       onClick={handleGoogleClick}
+      type="button"
       className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
     >
-      Continue With Google
+      Continue with google
     </button>
   );
 }
