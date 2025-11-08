ec2c6b2e49b4ebeba420ad5029d415aecfce7806
Abhishek <kumarabhishek32202@gmail.com>
2025-11-09 03:54:19 +0530

    complete user functionality

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 dotenv.config();
 // mongoose.connect(process.env.MONGO);
@@ -11,7 +12,10 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+
 app.use(express.json())
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000!`);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,15 @@
 import express from "express";
 import {
-  test
+  test,
+  updateUser
 } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyuser.js";
 // import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-// router.post("/update/:id", verifyToken, updateUser);
+router.post("/update/:id", verifyToken, updateUser);
 // router.delete("/delete/:id", verifyToken, deleteUser);
 // router.get("/listings/:id", verifyToken, getUserListings);
 // router.get("/:id", verifyToken, getUser);
