5da18ca6624a1ef3119c4a15ff14c96b37ee68f1
Abhishek Kumar <kumarabhishek32202@gmail.com>
2026-01-17 08:14:43 +0530

    Add contact landlord functionality to the listing

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,9 +4,10 @@ import {
   test,
   updateUser,
   getUserListings,
+  getUser
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyuser.js";
-// import { verifyToken } from "../utils/verifyUser.js";
+
 
 const router = express.Router();
 
@@ -14,6 +15,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
-// router.get("/:id", verifyToken, getUser);
+router.get("/:id", verifyToken, getUser);
 
 export default router;
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,63 +1,52 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 export default function Contact({ listing }) {
-    const [landLord, setLandlord] = useState(null);
-    const [message, setMessage] = useState("");
-    const handleChange = (e) => {
-        setMessage(e.target.value);
+  const [landlord, setLandlord] = useState(null);
+  const [message, setMessage] = useState("");
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  useEffect(() => {
+    const fetchLandlord = async () => {
+      try {
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        setLandlord(data);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    useEffect(() => {
-        const fetchLandLord = async () => {
-            try {
-                const res = await fetch(
-                    `https://plotpe-mern-project.onrender.com/api/user/${listing.userRef}`,
-                    {
-                        credentials: "include",
-                    }
-                );
-                const data = await res.json();
-                if (data.success === false) {
-                    console.log("Error in pulling landlord details");
-                    return;
-                }
-                console.log("landlord details", data);
-                setLandlord(data);
-            } catch (err) {
-                console.log("Error in pulling landlord details", err);
-            }
-        };
-        
+    fetchLandlord();
+  }, [listing.userRef]);
+  return (
+    <>
+      {landlord && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={onChange}
+            placeholder="Enter your message here..."
+            className="w-full border p-3 rounded-lg"
+          ></textarea>
 
-        fetchLandLord();
-    }, [listing.userRef]);
-
-    return (
-        <>
-            {landLord && (
-                <div className="flex flex-col gap-2">
-                    <p>
-                        Contact <span className="font-semibold">{landLord.username}</span>{" "}
-                        for{" "}
-                        <span className="font-semibold">{listing.name.toLowerCase()}</span>
-                    </p>
-                    <textarea
-                        name="message"
-                        id="message"
-                        rows="2"
-                        value={message}
-                        onChange={handleChange}
-                        placeholder="Enter your message here..."
-                        className="w-full border p-3 rounded-lg"
-                    />
-                    <Link
-                        to={`mailto:${landLord.email}?subject=Regarding ${listing.name}&body=${message} `}
-                        className="bg-green-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
-                    >
-                        Send Message
-                    </Link>
-                </div>
-            )}
-        </>
-    );
+          <Link
+            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
