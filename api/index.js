61cf1161dc1c80ff28252618728ac01ef2a814eb
Abhishek Kumar <kumarabhishek32202@gmail.com>
2026-02-01 05:15:52 +0530

    added pre installatione part

--- api/index.js
+++ api/index.js
@@ -27,9 +27,12 @@ app.use(express.json())
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000!`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
