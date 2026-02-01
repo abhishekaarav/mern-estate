1071ec2c740291bcef3937d0076bbf5936f580b1
Abhishek Kumar <kumarabhishek32202@gmail.com>
2026-02-01 06:01:37 +0530

    fix express wildcard route

--- api/index.js
+++ api/index.js
@@ -40,9 +40,10 @@ app.use('/api/listing', listingRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
